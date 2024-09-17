33fa89b5f8043428aa702a93f7b0ea511364b068
Arthur Guerra <arthurguerra.anastacio0@gmail.com>
2024-09-17 08:43:12 -0300

    PRODUCTS 17-09

--- pizzaria-guidelicia/src/App.js
+++ pizzaria-guidelicia/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header';
 import SecaoCapa from './components/SecaoCapa';
+import Produtos from './components/Produtos';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <SecaoCapa/>
+      <Produtos/>
     </div>
   );
 }
---
+++ pizzaria-guidelicia/src/components/Produtos/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './style.css';
+
+const Produtos = () => {
+   return (
+     <section id="produtos">
+         <h2 className="titulos"> Nossos Produtos</h2>
+         <p>
+           Temos as melhores pizzas do litoral
+         </p>
+
+         <p>
+           Contém es melhores pizzes de Litorel e com preçes acessíveis pre todes!
+         </p>
+
+     </section>
+
+  )
+}
+
+export default Produtos;
