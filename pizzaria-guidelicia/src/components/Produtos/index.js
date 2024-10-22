05be07ca6a7e9b6ef5f3f4ab7e396450333e39a5
Arthur <arthurguerraanastacio0@gmail.com>
2024-10-22 08:48:10 -0300

    PROCUCTS 22-10

--- pizzaria-guidelicia/src/components/Produtos/index.js
+++ pizzaria-guidelicia/src/components/Produtos/index.js
@@ -2,20 +2,36 @@ import React from "react";
 import './style.css';
 
 const Produtos = () => {
-   return (
-     <section id="produtos">
-         <h2 className="titulos"> Nossos Produtos</h2>
-         <p>
-           Temos as melhores pizzas do litoral
-         </p>
+  return (
+    <div className="produtos">
+      <h1>NOSSOS PRODUTOS</h1>
+      <p>
+        Trabalhamos com vendas de carros novos e semi-novos, a maioria de nosso estoque é relacionada ao VolksWagen Fusca.
+        Todos os nossos preços são acessíveis e contam com a melhor qualidade do mercado!
+      </p>
 
-         <p>
-           Contém es melhores pizzes de Litorel e com preçes acessíveis pre todes!
-         </p>
+      <div className="carros-antigos">
+        <h2></h2>
+        <div className="carro">
+          <img src="./assets/pizza_portuguesa.png" alt="Pizza de Portugesa" />
 
-     </section>
-
-  )
+          <span>xx</span>
+        </div>
+        <div className="carro">
+          <img src="./assets/pizza_frango.png" alt="Pizza de Frango" />
+          <span>xx</span>
+        </div>
+        <div className="carro">
+          <img src="./assets/pizza_clabresa.png" alt="Pizza de Calabresa" />
+          <span>xx</span>
+        </div>
+        <div className="carro">
+          <img src="./assets/pizza_strogonoff.png" alt="Pizza de Strogonoff" />
+          <span>xx</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Produtos;
